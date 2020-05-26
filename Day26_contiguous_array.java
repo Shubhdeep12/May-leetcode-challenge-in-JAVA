760928ccf87b6624697f0883476b4c4c0f1e1d7d
Shubhdeep Chhabra <43654389+Shubhdeep12@users.noreply.github.com>
2020-05-26 13:42:47 +0530

    Add files via upload

---
+++ Day26_contiguous_array.java
@@ -0,0 +1,19 @@
+public class Solution {
+
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int max= 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                max = Math.max(max, i - map.get(count));
+                
+            } else {
+                map.put(count, i);
+            }
+            //System.out.println(count+" "+i);
+        }
+        return max;
+    }
+}
