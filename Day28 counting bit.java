3929dd3fbd038d352d1907b441ba9ff630385010
Shubhdeep Chhabra <43654389+Shubhdeep12@users.noreply.github.com>
2020-05-28 14:21:40 +0530

    Add files via upload

---
+++ Day28 counting bit.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] countBits(int num) {
+        int[] res=new int[num+1];
+        for(int i=0;i<=num;i++){
+           
+            res[i]=Integer.bitCount(i);
+        }
+        return res;
+    }
+}
